bd6556d5423be895c951527a5e1802f2d64c1733
Kelerm <kazakaa@gmail.com>
2018-04-06 09:51:40 +0200

    feat: add JSP page

--- src/fr/kelerm/servlet/Test.java
+++ src/fr/kelerm/servlet/Test.java
@@ -9,23 +9,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Test extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        response.setCharacterEncoding("UTF-8");
-        PrintWriter out = response.getWriter();
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<meta charset=\"utf-8\" />");
-        out.println("<title>Test</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<p>Ceci est une page générée depuis une servlet.</p>");
-        out.println("</body>");
-        out.println("</html>");
+        this.getServletContext().getRequestDispatcher( "/WEB-INF/test.jsp" ).forward( request, response );
     }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {}
+
 }
