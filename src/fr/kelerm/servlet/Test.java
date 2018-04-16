1f8829f5fad5f4d44069e4e48684576dddb7b0cd
Kelerm <kazakaa@gmail.com>
2018-04-16 10:38:36 +0200

    feat: test liste + boucle en EL

--- src/fr/kelerm/servlet/Test.java
+++ src/fr/kelerm/servlet/Test.java
@@ -1,11 +1,15 @@
 package fr.kelerm.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.joda.time.DateTime;
 
 import fr.kelerm.bean.Person;
 
@@ -15,14 +19,31 @@ public class Test extends HttpServlet {
         /* Création et initialisation du message. */
         String paramAuteur = request.getParameter("auteur");
         String message = "Tranmission de variables : OK ! " + paramAuteur;
-        /* Création du bean */
+        /* Création du bean et Initialisation de ses propriétés */
         Person premierBean = new Person();
-        /* Initialisation de ses propriétés */
         premierBean.setNom("Kelerm");
         premierBean.setPrenom("Krom");
+
+        /* Création de la liste et insertion de quatre éléments */
+        List<Integer> premiereListe = new ArrayList<Integer>();
+        premiereListe.add(27);
+        premiereListe.add(12);
+        premiereListe.add(138);
+        premiereListe.add(6);
+
+        /** On utilise ici la libraire Joda pour manipuler les dates, pour deux raisons :
+         *    - c'est tellement plus simple et limpide que de travailler avec les objets Date ou Calendar !
+         *    - c'est (probablement) un futur standard de l'API Java.
+         */
+        DateTime dt = new DateTime();
+        Integer jourDuMois = dt.getDayOfMonth();
+
         /* Stockage du message et du bean dans l'objet request */
         request.setAttribute("test", message);
         request.setAttribute("kelerm", premierBean);
+        request.setAttribute("liste", premiereListe);
+        request.setAttribute("jour", jourDuMois);
+
         /* Transmission de la paire d'objets request/response à notre JSP */
         this.getServletContext().getRequestDispatcher("/WEB-INF/test.jsp").forward(request, response);
     }
--- web/WEB-INF/test.jsp
+++ web/WEB-INF/test.jsp
@@ -1,4 +1,5 @@
 <%@ page pageEncoding="UTF-8" %>
+<%@ page import="java.util.List" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,20 +10,32 @@
 <body>
 <p>Ceci est une page générée depuis une JSP.</p>
 <p>
-    <%
-        String attribut = (String) request.getAttribute("test");
-        out.println( attribut );
-
-        String parametre = request.getParameter( "auteur" );
-        out.println( parametre );
-    %>
+    ${test}
+    ${param.auteur}
 </p>
 <p>
     Récupération du bean :
+    ${kelerm.prenom}
+    ${kelerm.nom}
+</p>
+<p>
+    Récupération de la liste :
     <%
-        fr.kelerm.bean.Person notreBean = (fr.kelerm.bean.Person) request.getAttribute("kelerm");
-        out.println( notreBean.getPrenom() );
-        out.println( notreBean.getNom() );
+        List<Integer> liste = (List<Integer>) request.getAttribute("liste");
+        for (Integer i : liste) {
+            out.println(i + " : ");
+        }
+    %>
+</p>
+<p>
+    Récupération du jour du mois :
+    <%
+        Integer jourDuMois = (Integer) request.getAttribute("jour");
+        if (jourDuMois % 2 == 0) {
+            out.println("Jour pair : " + jourDuMois);
+        } else {
+            out.println("Jour impair : " + jourDuMois);
+        }
     %>
 </p>
 </body>
